c629226206ea37235227322ce8c15de50f1cd259
emmanuel <entibimenya03@gmail.com>
2025-02-19 12:45:39 +0000

    more on piece of state with open and close button

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   //First we add a new state variable, 2. we use it the code 3. we update the piece of state in some event handler
   //note that useState function is known as a hook; we can identify a hook by use... and also you can only call a hook on the top level of the component function; also not in the if statement
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
   //event handler function right in the component
   function handlePrevious() {
     // alert("Previous");
@@ -25,38 +26,44 @@ function App() {
   }
   return (
     <>
-      <div className="steps">
-        <div className="numbers">
-          <div className={step >= 1 ? "active" : ""}>1</div>
-          <div className={step >= 2 ? "active" : ""}>2</div>
-          <div className={step >= 3 ? "active" : ""}>3</div>
-        </div>
+      {/*updating the setIsOpen()*/}
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-        <p className="message">
-          {/* -1 because the array is zero based*/}
-          {step}:{messages[step - 1]}
-        </p>
-        <div className="buttons">
-          {/*handling event in react way: click is the event name and we also add on, then we pass in a function; we add an event listerner right where it is going to happen
+          <p className="message">
+            {/* -1 because the array is zero based*/}
+            {step}:{messages[step - 1]}
+          </p>
+          <div className="buttons">
+            {/*handling event in react way: click is the event name and we also add on, then we pass in a function; we add an event listerner right where it is going to happen
           the function will be executed whenever there is a click on this DOM element
           it is very important to stress that it is a callback function not a function call like this onClick={alert('Test)}*/}
-          <buton
-            style={{ backgroundColor: "#7950f2", color: "#fff" }}
-            // onClick={() => alert("Previous")}
-            //onMouseEnter={() => alert("Test")}
-            onClick={handlePrevious}
-          >
-            Previous
-          </buton>
-          <buton
-            style={{ backgroundColor: "#7950f2", color: "#fff" }}
-            // onClick={() => alert("Next")}
-            onClick={handleNext}
-          >
-            Next
-          </buton>
+            <buton
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              // onClick={() => alert("Previous")}
+              //onMouseEnter={() => alert("Test")}
+              onClick={handlePrevious}
+            >
+              Previous
+            </buton>
+            <buton
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              // onClick={() => alert("Next")}
+              onClick={handleNext}
+            >
+              Next
+            </buton>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
